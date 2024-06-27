aa3ce150ecd13e0c2abd9da17fd7cfa79de1c0b8
DaoudAnas13 <daoudanas28@gmail.com>
2024-06-27 10:22:25 +0100

    Initial version of store generated by JHipster-6.10.5

---
+++ src/main/java/com/redzone/store/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.redzone.store.config.audit;
---
+++ src/main/java/com/redzone/store/repository/ProductRepository.java
@@ -0,0 +1,14 @@
+package com.redzone.store.repository;
+
+import com.redzone.store.domain.Product;
+
+import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Spring Data  repository for the Product entity.
+ */
+@SuppressWarnings("unused")
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
